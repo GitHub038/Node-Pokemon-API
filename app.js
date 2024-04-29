a6c01d235bfee65b74aafb5253dc4c831281374c
sao <c_pasmoi@outlook.com>
2024-04-29 13:20:52 +0200

    add routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express"); // require permet de récupérer la dépendance express depuis node_modules
+let pokemons = require("./mock-pokemon");
 
 const app = express(); // création d'une instance d'une application express (serveur web sur lequel va fonctionner notre API Rest)
 const port = 3000; // port sur lequel l'API Rest va démarrer
@@ -6,6 +7,18 @@ const port = 3000; // port sur lequel l'API Rest va démarrer
 app.get("/", (req, res) => res.send("Hello, Express !")); // 1er Endpoint
 // 2eme argument est une fonction dont le rôle est de fournir une réponse au client lorsque l'on sera sur la route "/"
 // "req" est l'objet de la requete reçue et "res" l'objet réponse envoyé depuis express au client
+
+app.get("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  res.send(`Vous avez demandé le pokémon ${pokemon.name}`);
+});
+
+app.get("/api/pokemons", (req, res) => {
+  res.send(
+    `Il y a ${pokemons.length} pokémons dans le pokédex pour le moment.`
+  );
+});
 
 app.listen(port, () =>
   // démarrage de l'API Rest sur le port 3000
