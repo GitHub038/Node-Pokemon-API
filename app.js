39b56f5081dc0dd00fa2575e3dd6b2f0d3ccfed8
sao <c_pasmoi@outlook.com>
2024-05-02 15:01:37 +0200

    add Middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express"); // require permet de récupérer la dépendance express depuis node_modules
+const morgan = require("morgan");
+const favicon = require("serve-favicon");
 const { success } = require("./helper.js");
 let pokemons = require("./mock-pokemon");
 
 const app = express(); // création d'une instance d'une application express (serveur web sur lequel va fonctionner notre API Rest)
 const port = 3000; // port sur lequel l'API Rest va démarrer
+
+app.use(favicon(__dirname + "/favicon.ico")).use(morgan("dev")); // morgan paramétré en fct des messages de log que l'on veut, ici "dev"
 
 app.get("/", (req, res) => res.send("Hello, Express !")); // 1er Endpoint
 // 2eme argument est une fonction dont le rôle est de fournir une réponse au client lorsque l'on sera sur la route "/"
